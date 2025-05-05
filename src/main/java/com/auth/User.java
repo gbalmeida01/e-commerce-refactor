b25edc880716cd17a69673a1fe3252212e31ab6b
Gabriel Almeida <gjalmeida0309@gmail.com>
2025-05-05 10:58:22 -0300

    User class

---
+++ src/main/java/com/auth/User.java
@@ -0,0 +1,62 @@
+package com.auth;
+
+public class User {
+    private int id;
+    private String userName;
+    private String email;
+    private String password;
+    private String role;
+
+    public User(int id, String userName, String email, String password, String role) {
+        this.id = id;
+        this.userName = userName;
+        this.email = email;
+        this.password = password;
+        this.role = role;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public String toString() {
+        return String.format("User{id=%d, username='%s', email='%s', role='%s'}",
+                id, userName, email, role);
+    }
+}
